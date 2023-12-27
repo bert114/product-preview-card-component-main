50e23ea88a474cd4ec3453002ad3a165e81df1c8
bert114 <janalbertpenano14@gmail.com>
2023-12-27 09:45:55 +0800

    adding file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import Main from "./react/components/main";
 import "./scss/main.scss";
 import "animate.css";
+import productImgMobile from "../src/assets/images/image-product-mobile.jpg";
+import productImgDesktop from "../src/assets/images/image-product-desktop.jpg";
 
 function App() {
-  return <h1>hi</h1>;
+  return (
+    <div className="card-wrapper">
+      <div className="container">
+        <picture>
+          <source media="(min-width: 600px)" srcset={productImgDesktop} />
+          <img src={productImgMobile} alt="" />
+        </picture>
+        <article>
+          <div>PERFUME</div>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>
+            A floral, solar and voluptuous interpretation composed by Oliver
+            Polge, Perfumer-Creator for the House of CHANEL{" "}
+          </p>
+          <div>
+            <div>$149.99</div> <span>$169.99</span>
+          </div>
+          <button>
+            <div className="icon"></div>Add to Cart
+          </button>
+        </article>
+      </div>
+    </div>
+  );
 }
 
 export default App;
